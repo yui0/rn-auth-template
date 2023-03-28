68811937fde0de39e886dac2f9de58dcb1d9cbaa
Yuichiro Nakada <berry@berry-lab.net>
2023-03-28 17:07:02 +0900

    update some packages

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ import Animated, {
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Ionicons } from '@expo/vector-icons';
 //import Icon from 'react-native-vector-icons/FontAwesome';
 //import { BeakerIcon } from '@heroicons/react/24/solid';
